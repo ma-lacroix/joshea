0759c6a20ce14d496aee4be9d835b537dc792fdb
ma-lacroix <marc.lacroix@booking.com>
2025-02-20 14:57:22 +0100

    Delete DAG unit tests

--- tests/controllers/test_delete_data.py
+++ tests/controllers/test_delete_data.py
@@ -16,3 +16,12 @@ class TestDeleteData(unittest.TestCase):
     @patch('app.controllers.delete_data.write_json', side_effect=FileNotFoundError)
     def test_flush_meta_data_fail(self, mock_write_json):
         self.assertEqual("Failed to delete data!", delete_data.flush_meta_data())
+
+    @patch('app.controllers.delete_data.write_json', return_value=None)
+    @patch('app.controllers.delete_data.get_json', return_value={"workflows": {"dag1.py": ['get_data']}})
+    def test_delete_workflow_pass(self, mock_write_json, mock_get_json):
+        self.assertEqual("Deleted dag1.py successfully", delete_data.delete_workflow('dag1.py'))
+
+    @patch('app.controllers.delete_data.get_json', side_effect=FileNotFoundError)
+    def test_delete_workflow_fail(self, mock_write_json):
+        self.assertEqual("Failed to delete dag1.py", delete_data.delete_workflow('dag1.py'))
